15e19bad3fc9a9178baee1520282c7fee00f16b2
Andrey Kondalov <andrey@kondalov.com>
2021-09-09 01:36:34 +0400

    refactor: styles linted

--- src/App.svelte
+++ src/App.svelte
@@ -21,8 +21,17 @@
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-      'Helvetica Neue', sans-serif;
+    font-family:
+      -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      'Open Sans',
+      'Helvetica Neue',
+      sans-serif;
   }
 
   main {
